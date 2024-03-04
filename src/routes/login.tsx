8e0206905c04110adbc5d94fae58de39dde5d6a8
letsummer <letsummerparty@gmail.com>
2024-03-04 14:21:31 +0900

    로그인, 회원가입 화면 구현

---
+++ src/routes/login.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { auth } from "./firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Input, Switcher, Title, Wrapper, Form, Error } from "../components/auth-components";
+
+export default function Login(){
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        const{target: {name, value}}=e;
+        if(name === "email"){
+            setEmail(value);
+        } else if(name === "password"){
+            setPassword(value);
+        }
+    }
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        setError("");
+        if(isLoading || email === "" || password === "") return;
+        try {
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        } catch (e) {
+            if (e instanceof FirebaseError) {
+               setError(e.message);
+            }
+        }
+        finally {
+            setLoading(false);
+        }
+        console.log(email, password);
+    }
+
+    return(
+        <Wrapper>
+            <Title>Log into Ⅹ</Title>
+            <Form onSubmit={onSubmit}>
+                <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+                <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required />
+                <Input type="submit" value={isLoading? "Loading...":"Login"} />
+            </Form>
+            {error !==""? <Error>{error}</Error>: null}
+            <Switcher>
+                Don't have an account? <Link to="/create-account">Create one &rarr;</Link>
+            </Switcher>
+        </Wrapper>
+    );
+}
