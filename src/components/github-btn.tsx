ec14dbdd9954bcdefb50d258e0e0739e50a7a06d
letsummer <letsummerparty@gmail.com>
2024-03-04 14:22:11 +0900

    깃허브 로그인 컴포넌트 구현

---
+++ src/components/github-btn.tsx
@@ -0,0 +1,42 @@
+import { GithubAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
+import styled from "styled-components"
+import { auth } from "../routes/firebase";
+import { useNavigate } from "react-router-dom";
+
+const Button = styled.span`
+    margin-top: 20px;
+    background-color: white;
+    font-weight: 600;
+    color: black;
+    width: 100%;
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: 0;
+    display: flex;
+    gap: 5px;
+    align-items: center;
+    justify-content: center;
+
+`;
+const Logo = styled.img`
+    height: 25px;
+`;
+
+export default function GithubButton(){
+    const navigate = useNavigate();
+    const onClick = async ()=>{
+        try {
+            const provider = new GithubAuthProvider();
+            await signInWithRedirect(auth, provider);
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <Button onClick={onClick}>
+            <Logo src="/github-logo.svg"/>
+            Continue with Github
+        </Button>
+    );
+}
